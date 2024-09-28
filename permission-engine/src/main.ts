5ff56d950365ef49d3fcd2133c1024d66467e48a
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-09-28 13:23:19 +0900

    swagger: fix api auth

--- permission-engine/src/main.ts
+++ permission-engine/src/main.ts
@@ -27,10 +27,26 @@ async function bootstrap() {
     .setDescription('API documentation of permissioning engine') // API에 대한 설명
     .setVersion('1.0') // 버전 정보
     .addBearerAuth()
+    .setBasePath('/api/v1')
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document); // '/api' 경로에 Swagger UI 설정
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      authAction: {
+        BearerAuth: {
+          name: 'Authorization',
+          schema: {
+            type: 'apiKey',
+            in: 'header',
+            name: 'Authorization',
+            description: 'Enter JWT Bearer token',
+          },
+          value: 'Bearer <your-token-here>',
+        },
+      },
+    },
+  });
 
   await app.listen(process.env.PERMISSION_ENGINE_PORT);
 }
