2c7e52855166031028dab44af8e42f65f0a46130
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-10-04 13:32:22 +0900

    permission-engine/src/app: add email-sender module

--- permission-engine/src/lib/email-sender/email-sender.module.ts
+++ permission-engine/src/lib/email-sender/email-sender.module.ts
@@ -2,14 +2,23 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 import { EmailSenderProcessor } from './email-sender.processor';
 import { EmailSenderService } from './email-sender.service';
+import { Logger } from '../logger/logger.service';
+import { SESService } from '../ses/ses.service';
+import { SESModule } from '../ses/ses.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'src/config/configuration';
 
 @Module({
   imports: [
-    BullModule.registerQueue({
-      name: 'EmailSender',
+    ConfigModule.forRoot({
+      load: [configuration],
     }),
+    BullModule.registerQueue({
+      name: 'email-sender',
+    }),
+    SESModule,
   ],
-  providers: [EmailSenderProcessor, EmailSenderService],
+  providers: [EmailSenderProcessor, EmailSenderService, SESService, Logger],
   exports: [EmailSenderService],
 })
 export class EmailSenderModule {}
