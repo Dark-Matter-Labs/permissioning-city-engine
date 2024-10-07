0581621815f12a1ad461f0684492293baf5f2b12
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-10-07 23:21:23 +0900

    permission-engine/src/api/user/dto/update-user.dto.ts: update update-user dto

--- permission-engine/src/api/user/dto/update-user.dto.ts
+++ permission-engine/src/api/user/dto/update-user.dto.ts
@@ -17,41 +17,41 @@ export class UpdateUserDto {
 
   @IsNotEmpty()
   @MaxLength(100)
-  @ApiProperty({ description: 'User name in string', required: true })
-  name: string;
+  @ApiProperty({ description: 'User name in string' })
+  name?: string;
 
   @IsEmail()
   @IsNotEmpty()
-  @ApiProperty({ description: 'User email', required: true })
-  email: string;
+  @ApiProperty({ description: 'User email' })
+  email?: string;
 
   @IsEnum(UserType)
   @ApiProperty({ description: 'User type', default: 'individual' })
-  type: 'individual' | 'organization' | 'government';
+  type?: 'individual' | 'organization' | 'government';
 
   @Matches(/^\d{4}$/, { message: 'The number must be a 4-digit number.' })
   @Min(1900)
   @IsNotEmpty()
-  @ApiProperty({ description: 'Year of birth in 4 digits', required: true })
-  birthYear: number;
+  @ApiProperty({ description: 'Year of birth in 4 digits' })
+  birthYear?: number;
 
   @IsNotEmpty()
   @MaxLength(100)
-  @ApiProperty({ description: 'Country', required: true })
-  country: string;
+  @ApiProperty({ description: 'Country' })
+  country?: string;
 
   @IsNotEmpty()
   @MaxLength(100)
-  @ApiProperty({ description: 'State|Region', required: true })
-  region: string;
+  @ApiProperty({ description: 'State|Region' })
+  region?: string;
 
   @IsNotEmpty()
   @MaxLength(100)
-  @ApiProperty({ description: 'City', required: true })
-  city: string;
+  @ApiProperty({ description: 'City' })
+  city?: string;
 
   @IsNotEmpty()
   @MaxLength(100)
-  @ApiProperty({ description: 'District', required: true })
-  district: string;
+  @ApiProperty({ description: 'District' })
+  district?: string;
 }
