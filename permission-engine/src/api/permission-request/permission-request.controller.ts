b491b1e434272d907b700a86c426bdc0a36e21f8
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-10-10 16:48:07 +0900

    permission-engine/src/api/permission-request/permission-request.controller.ts: add constraints on create

--- permission-engine/src/api/permission-request/permission-request.controller.ts
+++ permission-engine/src/api/permission-request/permission-request.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   ForbiddenException,
@@ -6,6 +7,7 @@ import {
   Param,
   Post,
   Query,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { CreatePermissionRequestDto } from './dto';
@@ -13,12 +15,20 @@ import { PermissionRequestService } from './permission-request.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { FindAllPermissionRequestDto } from './dto';
+import { SpaceEventService } from '../space-event/space-event.service';
+import { SpaceService } from '../space/space.service';
+import { UserService } from '../user/user.service';
+import { SpacePermissionerService } from '../space-permissioner/space-permissioner.service';
 
 @ApiTags('permission')
 @Controller('api/v1/permission/request')
 export class PermissionRequestController {
   constructor(
     private readonly permissionRequestService: PermissionRequestService,
+    private readonly spaceService: SpaceService,
+    private readonly userService: UserService,
+    private readonly spaceEventService: SpaceEventService,
+    private readonly spacePermissionerService: SpacePermissionerService,
   ) {}
 
   @Get()
@@ -52,7 +62,39 @@ export class PermissionRequestController {
   @Post()
   @ApiOperation({ summary: 'Create PermissionRequest' })
   @UseGuards(JwtAuthGuard)
-  create(@Body() createPermissionRequestDto: CreatePermissionRequestDto) {
+  async create(
+    @Req() req,
+    @Body() createPermissionRequestDto: CreatePermissionRequestDto,
+  ) {
+    const user = await this.userService.findOneByEmail(req.user.email);
+    const { spaceEventId, spaceRuleId, spaceId } = createPermissionRequestDto;
+
+    if (spaceEventId != null && spaceRuleId != null) {
+      throw new BadRequestException(
+        'cannot create permission request with multiple purposes',
+      );
+    } else if (spaceEventId != null) {
+      // space event permission request
+      const spaceEvent = await this.spaceEventService.findOneById(spaceEventId);
+
+      if (spaceEvent.organizerId !== user.id) {
+        throw new ForbiddenException('user must be an event organizer');
+      }
+    } else if (spaceRuleId != null) {
+      // space rule change permission request
+      const isSpacePermissioner =
+        await this.spacePermissionerService.isSpacePermissioner(
+          spaceId,
+          user.id,
+        );
+
+      if (isSpacePermissioner === false) {
+        throw new ForbiddenException('user must be a space permissioner');
+      }
+    } else {
+      throw new BadRequestException();
+    }
+
     return this.permissionRequestService.create(createPermissionRequestDto);
   }
 
