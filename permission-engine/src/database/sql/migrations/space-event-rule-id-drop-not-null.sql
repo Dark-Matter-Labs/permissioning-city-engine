36abd8d64fb94a15381ea94c7762e50ef22445ba
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-10-15 11:51:04 +0900

    permission-engine/src/database/sql/migrations/space-event-rule-id-drop-not-null.sql: run query only if needed

--- permission-engine/src/database/sql/migrations/space-event-rule-id-drop-not-null.sql
+++ permission-engine/src/database/sql/migrations/space-event-rule-id-drop-not-null.sql
@@ -1,2 +1,9 @@
-ALTER TABLE space_event
-ALTER COLUMN rule_id DROP NOT NULL;
+DO $$ BEGIN IF EXISTS (
+  SELECT 1
+  FROM information_schema.columns
+  WHERE table_name = 'space_event'
+    AND column_name = 'rule_id'
+    AND is_nullable = 'NO'
+) THEN EXECUTE 'ALTER TABLE space_event ALTER COLUMN rule_id DROP NOT NULL';
+END IF;
+END $$;
