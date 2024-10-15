a6eda096cafe0b068f8658caf96dd9596d581bf7
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-10-15 11:06:18 +0900

    scripts/deploy.sh: fix command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 git pull origin prod
 git submodule update
 
-docker-compose down
-docker-compose -f docker-compose.prod.yml up -d --build
+docker compose down
+docker compose -f docker-compose.prod.yml up -d --build
 docker system prune -f
 
 echo "Deployment completed."
