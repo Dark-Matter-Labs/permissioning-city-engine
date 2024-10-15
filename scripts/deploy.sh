8ed89262da9a65d6ce9414ddd4b2329ff26ab05f
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-10-15 10:59:41 +0900

    add auto deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git pull origin prod
+git submodule update
+
+docker-compose down
+docker-compose -f docker-compose.prod.yml up -d --build
+docker system prune -f
+
+echo "Deployment completed."
