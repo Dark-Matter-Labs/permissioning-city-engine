f514a5ca9ab4254971c869a0060d3b9f4cea6bd8
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-10-17 15:25:57 +0900

    permission-engine/src/lib/util/util.ts: add generateRandomCode

--- permission-engine/src/lib/util/util.ts
+++ permission-engine/src/lib/util/util.ts
@@ -3,3 +3,17 @@ import * as crypto from 'crypto';
 export const hash = (str: string) => {
   return crypto.createHash('sha256').update(str).digest('hex');
 };
+
+export const generateRandomCode = (length = 8) => {
+  const characters =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const bytes = crypto.randomBytes(length);
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = bytes[i] % characters.length;
+    result += characters[randomIndex];
+  }
+
+  return result;
+};
