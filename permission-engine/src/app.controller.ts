0f34013f75b4204d52500df24d814045845c0233
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-10-17 15:25:21 +0900

    permission-engine/src/app.controller.ts: remove unused controller

--- permission-engine/src/app.controller.ts
+++ permission-engine/src/app.controller.ts
@@ -1,20 +1,7 @@
 // src/app.controller.ts
-import { Controller, Post, Body } from '@nestjs/common';
-import { EmailSenderService } from './lib/email-sender/email-sender.service';
-import { EmailSendJobData } from './lib/type';
-import { Logger } from './lib/logger/logger.service';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly emailSenderService: EmailSenderService,
-    private readonly logger: Logger,
-  ) {}
-
-  @Post('add-job')
-  async addJob(@Body() data: EmailSendJobData) {
-    await this.emailSenderService.addJob(data);
-    this.logger.log(`Daemon Job request added to the queue: ${data.targetId}`);
-    return `Daemon request added to the queue: ${data.targetId}`;
-  }
+  constructor() {}
 }
