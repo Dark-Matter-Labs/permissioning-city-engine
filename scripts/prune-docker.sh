846e0286e461c9abab14697b19102b41c9ec80a1
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-11-21 13:44:50 +0900

    scripts/prune-docker.sh: fix script

--- scripts/prune-docker.sh
+++ scripts/prune-docker.sh
@@ -3,16 +3,16 @@
 echo "Running: docker system prune -f -a"
 docker system prune -f -a
 
-echo "Running: docker container prune"
-docker container prune
+echo "Running: docker container prune -f"
+docker container prune -f
 
-echo "Running: docker image prune -a"
-docker image prune -a
+echo "Running: docker image prune -f -a"
+docker image prune -f -a
 
-echo "Running: docker volume prune -a"
-docker volume prune -a
+echo "Running: docker volume prune -f -a"
+docker volume prune -f -a
 
-echo "Running: docker builder prune"
-docker builder prune
+echo "Running: docker builder prune -f"
+docker builder prune -f
 
 echo "Docker prune completed."
