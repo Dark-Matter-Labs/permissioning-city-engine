05c7c3eff140987c2ffd7a8e14f0fddbf874a25a
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-12-04 15:57:04 +0900

    docker-compose: amend dev environment

--- permission-engine/docker-entrypoint.sh
+++ permission-engine/docker-entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 
+echo "Install packages..."
 npm i
+echo "Package installation complete!"
 
-npm i -g pm2@5.4.3
-
+echo "Building application..."
 npm run build
+echo "Application build complete!"
 
 if [ "$NODE_ENV" = "production" ]; then
   echo "Production environment detected, serving the app..."
   npm run start:prod
 else
   echo "Non-production environment detected, tailing /dev/null..."
+  npm i -g pm2@5.4.3
   pm2 start --name "permission-engine" npm -- run start:dev
   pm2 logs
   tail -f /dev/null
