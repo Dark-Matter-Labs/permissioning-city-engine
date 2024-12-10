0c9076d85d5f5eba42f3143686bfa08a48e994ae
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-12-10 13:59:40 +0900

    certbot/docker-entrypoint.sh: ignore init error

--- certbot/docker-entrypoint.sh
+++ certbot/docker-entrypoint.sh
@@ -6,6 +6,8 @@ echo "dns_cloudflare_email = $EMAIL_CERTIFICATOR" > /etc/cloudflare/cloudflare.i
 echo "dns_cloudflare_api_key = $CLOUDFLARE_API_KEY" >> /etc/cloudflare/cloudflare.ini && \
 chmod 600 /etc/cloudflare/cloudflare.ini
 
+set +e
+
 # Request the certificate for the given domains
 certbot certonly \
   --dns-cloudflare \
@@ -16,6 +18,8 @@ certbot certonly \
   -d engine.permissioning.city \
   -d "*.engine.permissioning.city" \
   --non-interactive --quiet
+
+set -e
 
 # Set up the renewal loop
 trap exit TERM; \
