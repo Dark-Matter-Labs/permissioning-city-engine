2c91c137a340d14e61be28691f7752ec41a76b41
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-12-10 15:05:42 +0900

    scripts/deploy.sh: build prod

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cd ..
 git pull origin prod
 git submodule update
 
-docker compose build --no-cache
+docker compose -f docker-compose.prod.yml build --no-cache
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d
 docker system prune -f -a
